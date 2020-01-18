5076dc8b97ac8207f669cf6370878ca2d56c8467
ThomasL-GD <59962750+ThomasL-GD@users.noreply.github.com>
2020-01-18 13:01:23 +0100

    var and while
    Variables definition and few tests which work with the while loop and the scanf function

---
+++ Violence_is_bad.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+	
+	int nPlayerLife = 100;
+	int nAdvLife = 100;
+	int nPlayerMP = 5;
+	int nAdvMP = 5;
+	int nPlayerPoison = 0;
+	int nAdvPoison = 0;
+	int nChoice = 0;
+	
+	while(nChoice !=1 && nChoice != 2){
+		
+		printf("test");
+		
+		scanf("%d",&nChoice);
+		
+	}
+	
+	printf("it works yay");
+	
+}
